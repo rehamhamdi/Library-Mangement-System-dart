1375061b632a261efcf73bcd634925bfc929ebbf
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2024-07-14 18:49:41 +0300

    Add files via upload

---
+++ Task_3/class.dart
@@ -0,0 +1,25 @@
+class Book {
+  String? title;
+  String? author;
+
+  void displayBookInfo() {
+    print("The Title of the book is: $title");
+    print("The Author of the book is: $author");
+  }
+}
+
+class Library {
+  List<Book> books = [];
+
+  void addBook(Book book) {
+    book._title;
+    books.add(book);
+  }
+
+  void displayAllBooks() {
+    books.forEach((Element) {
+      Element.displayBookInfo();
+    });
+  }
+}
+
