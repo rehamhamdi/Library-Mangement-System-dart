0bd156f713eb7c518bcab11f9308b365b023a2c6
Reham Hamdi <111997040+rehamhamdi@users.noreply.github.com>
2024-07-14 19:28:52 +0300

    Update main.dart

--- Task_3/main.dart
+++ Task_3/main.dart
@@ -4,12 +4,10 @@ void main() {
   Book book1 = new Book();
   book1.title = "Harry Potter";
   book1.author = "J.K. Rowling";
-  book1.displayBookInfo();
 
   Book book2 = new Book();
   book2.title = "The Great Gatsby";
   book2.author = "F. Scott Fitzgerald";
-  book2.displayBookInfo();
 
   Library library = new Library();
   library.addBook(book1);
